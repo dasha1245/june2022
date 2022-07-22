677f9399f1952ef1eff2416d39c1d71d6f7bd8e3
dasha1245 <dcherkasova75@gmail.com>
2022-07-22 16:54:35 +0300

    init commit

---
+++ JS/lesson1/main.js
@@ -0,0 +1,89 @@
+// task1
+
+// let a = 'hello ';
+// console.log(a);
+// alert(a);
+// document.write(a);
+//
+// let b = 'owu ';
+// console.log(b);
+// alert(b);
+// document.write(b);
+//
+// let c = 'com ';
+// console.log(c);
+// alert(c);
+// document.write(c);
+//
+// let d = 'ua ';
+// console.log(d);
+// alert(d);
+// document.write(d);
+//
+// let e = 1 ;
+// console.log(e);
+// alert(e);
+// document.write(e);
+//
+// let f = 10 ;
+// console.log(f);
+// alert(f);
+// document.write(f);
+//
+// let g = -999 ;
+// console.log(g);
+// alert(g);
+// document.write(g);
+//
+// let h = 3.14 ;
+// console.log(h);
+// alert(h);
+// document.write(h);
+//
+// let i = 2.7 ;
+// console.log(i);
+// alert(i);
+// document.write(i);
+//
+// let j = 16 ;
+// console.log(j);
+// alert(j);
+// document.write(j);
+//
+// let k = true ;
+// console.log(k);
+// alert(k);
+// document.write(k);
+
+// let l = false ;
+// console.log(l);
+// alert(l);
+// document.write(l);
+
+//Task2
+
+// let firstName = `Дар'я `;
+// let middleName = 'Андріївна ';
+// let lastName = 'Черкасова '
+//
+// let person = lastName + firstName + middleName;
+// alert(person);
+
+//Task3
+
+// let a = 100;
+// let b = '100';
+// let c = true;
+// console.log(typeof a);
+// console.log(typeof b);
+// console.log(typeof c);
+
+//Task4
+
+// let firstName = prompt('What is your first name?', );
+// let middleName = prompt('What is your middle name?', );
+// let age = prompt('How old are you?', );
+//
+// console.log(firstName);
+// console.log(middleName);
+// console.log(age);
