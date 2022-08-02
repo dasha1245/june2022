bf974e46179e45bb66c01e58b56d5873a9c011cb
dasha1245 <dcherkasova75@gmail.com>
2022-08-03 00:22:16 +0300

    js lesson4

---
+++ JS/lesson 4/main.js
@@ -0,0 +1,148 @@
+// - За допомогою циклу for і document.write() вивести 10 блоків div c довільним текстом всередині
+
+// for (let i = 0; i < 11; i++ ){
+//     document.write("<div>Hi</div>")
+// }
+
+// - За допомогою циклу for і document.write() вивести 10 блоків div c довільним текстом і індексом всередині
+
+// for (let i = 0; i < 11; i++){
+//     document.write(`<div> Hi ${i}</div>`)
+// }
+
+// - За допомогою циклу while вивести в документ 20 блоків h1 c довільним текстом всередині.
+
+// let i = 0;
+// while (i < 21){
+//   document.write('<h1> Hi! </h1>')
+//     i++
+// }
+
+// - За допомогою циклу while вивести в документ 20 блоків h1 c довільним текстом і індексом всередині.
+
+// let i = 0;
+// while (i < 21){
+//     document.write(`<h1>Hi ${i}</h1>`)
+//     i++
+// }
+
+// - Використовуючи данні з масиву, за допомоги document.write та циклу
+// побудувати структуру по шаблону
+// Масив:
+//
+//     let listOfItems = ['html', 'css', 'javascript', 'mysql', 'mongodb', 'react', 'angular', 'node.js'];
+//
+// ШАБЛОН:
+//     <ul>
+//         <li>ITEM OF ARRAY</li>
+//         <!--
+//             і тд інші об'єкти масиву
+//              ...
+//              ...
+//              ...
+//         -->
+//     </ul>
+//
+// замість 'ITEM OF ARRAY' підставити елемент з масиву щоб получився цілий список з даними з масиву
+
+// let listOfItems = ['html', 'css', 'javascript', 'mysql', 'mongodb', 'react', 'angular', 'node.js'];
+// for (let item of listOfItems){
+//     document.write(`
+//         <ul>
+//             <li>${item}</li>
+//         </ul>
+//     `)
+// }
+
+// -----------------------------------------------
+
+//     Використовуючи данні з масиву, за допомоги document.write та циклу
+// побудувати структуру по шаблону  Зробити адекватну стилізацію
+// Великими літерами прописанні властивості об'єкту які потрібно впровадити в шаблон
+
+// let products = [
+//     {
+//         title: 'Milk',
+//         price: 22,
+//         image: 'https://www.mcqueensdairies.co.uk/wp-content/uploads/2019/02/Mcqueens_1litre_whole_organic-300x300-3.jpg'
+//     },
+//     {
+//         title: 'Juice',
+//         price: 27,
+//         image: 'https://images-na.ssl-images-amazon.com/images/I/61jL2GCuKLL._SX679_PIbundle-24,TopRight,0,0_AA679SH20_.jpg'
+//     },
+//     {
+//         title: 'Tomato',
+//         price: 47,
+//         image: 'https://dictionary.cambridge.org/ru/images/thumb/tomato_noun_001_17860.jpg?version=5.0.74'
+//     },
+//     {
+//         title: 'Tea',
+//         price: 15,
+//         image: 'https://yogiproducts.com/wp-content/uploads/2009/03/YT-US-CAR-RelaxedMind-C23-202201-V2-3DFront_withGlow-300DPI-1.png'
+//     },
+// ];
+//
+// for (product of products){
+//         // console.log(key, product[key])
+//         document.write(`
+//
+//             <div class="product-card">
+//                 <h3 class="product-title"> ${product.title}. Price - ${product.price}</h3>
+//                 <img src = " ${product.image} " alt="" class="product-image">
+//             </div>
+//
+//         `)
+// }
+
+// ШАБЛОН
+// <div class="product-card">
+//     <h3 class="product-title">TITLE. Price - PRICE</h3>
+// <img src="IMAGE" alt="" class="product-image">
+// </div>
+// Замість TITLE PRICE IMAGE - підставити відповідні поля з об'єкту
+//
+//
+//
+// --------------------
+//     є масив
+
+// let users = [
+//     {name: 'vasya', age: 31, status: false},
+//     {name: 'petya', age: 30, status: true},
+//     {name: 'kolya', age: 29, status: true},
+//     {name: 'olya', age: 28, status: false},
+//     {name: 'max', age: 30, status: true},
+//     {name: 'anya', age: 31, status: false},
+//     {name: 'oleg', age: 28, status: false},
+//     {name: 'andrey', age: 29, status: true},
+//     {name: 'masha', age: 30, status: true},
+//     {name: 'olya', age: 31, status: false},
+//     {name: 'max', age: 31, status: true}
+// ];
+
+
+// за допомоги циклу вивести:
+// - користувачів зі статусом true
+
+// for (user of users){
+//     if(user.status){
+//         console.log(user.name)
+//     }
+// }
+
+// - користувачів зі статусом false
+
+// for (user of users){
+//     if(!user.status){
+//         console.log(user.name)
+//     }
+// }
+
+// - користувачів які старші за 30 років
+
+// for (user of users){
+//     if(user.age > 30){
+//         console.log(user.name)
+//     }
+// }
